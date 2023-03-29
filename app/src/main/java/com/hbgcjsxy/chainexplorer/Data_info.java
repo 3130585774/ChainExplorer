1d03c42262ea3541786bc14887354f91b30c9937
spf <3070255392@qq.com>
2023-03-29 15:58:07 +0800

    Data_info

---
+++ app/src/main/java/com/hbgcjsxy/chainexplorer/Data_info.java
@@ -0,0 +1,133 @@
+package com.hbgcjsxy.chainexplorer;
+
+public class Data_info
+{
+    private String chainFullName;
+
+    private String chainShortName;
+
+    private String symbol;
+
+    private String rank;
+
+    private boolean mineable;
+
+    private String algorithm;
+
+    private String consensus;
+
+    private String diffEstimation;
+
+    private String currentDiff;
+
+    private String diffAdjustTime;
+
+    private String circulatingSupply;
+
+    private String totalSupply;
+
+    private String tps;
+
+    private String lastHeight;
+
+    private String lastBlockTime;
+
+    private String issueDate;
+
+    public void setChainFullName(String chainFullName){
+        this.chainFullName = chainFullName;
+    }
+    public String getChainFullName(){
+        return this.chainFullName;
+    }
+    public void setChainShortName(String chainShortName){
+        this.chainShortName = chainShortName;
+    }
+    public String getChainShortName(){
+        return this.chainShortName;
+    }
+    public void setSymbol(String symbol){
+        this.symbol = symbol;
+    }
+    public String getSymbol(){
+        return this.symbol;
+    }
+    public void setRank(String rank){
+        this.rank = rank;
+    }
+    public String getRank(){
+        return this.rank;
+    }
+    public void setMineable(boolean mineable){
+        this.mineable = mineable;
+    }
+    public boolean getMineable(){
+        return this.mineable;
+    }
+    public void setAlgorithm(String algorithm){
+        this.algorithm = algorithm;
+    }
+    public String getAlgorithm(){
+        return this.algorithm;
+    }
+    public void setConsensus(String consensus){
+        this.consensus = consensus;
+    }
+    public String getConsensus(){
+        return this.consensus;
+    }
+    public void setDiffEstimation(String diffEstimation){
+        this.diffEstimation = diffEstimation;
+    }
+    public String getDiffEstimation(){
+        return this.diffEstimation;
+    }
+    public void setCurrentDiff(String currentDiff){
+        this.currentDiff = currentDiff;
+    }
+    public String getCurrentDiff(){
+        return this.currentDiff;
+    }
+    public void setDiffAdjustTime(String diffAdjustTime){
+        this.diffAdjustTime = diffAdjustTime;
+    }
+    public String getDiffAdjustTime(){
+        return this.diffAdjustTime;
+    }
+    public void setCirculatingSupply(String circulatingSupply){
+        this.circulatingSupply = circulatingSupply;
+    }
+    public String getCirculatingSupply(){
+        return this.circulatingSupply;
+    }
+    public void setTotalSupply(String totalSupply){
+        this.totalSupply = totalSupply;
+    }
+    public String getTotalSupply(){
+        return this.totalSupply;
+    }
+    public void setTps(String tps){
+        this.tps = tps;
+    }
+    public String getTps(){
+        return this.tps;
+    }
+    public void setLastHeight(String lastHeight){
+        this.lastHeight = lastHeight;
+    }
+    public String getLastHeight(){
+        return this.lastHeight;
+    }
+    public void setLastBlockTime(String lastBlockTime){
+        this.lastBlockTime = lastBlockTime;
+    }
+    public String getLastBlockTime(){
+        return this.lastBlockTime;
+    }
+    public void setIssueDate(String issueDate){
+        this.issueDate = issueDate;
+    }
+    public String getIssueDate(){
+        return this.issueDate;
+    }
+}
