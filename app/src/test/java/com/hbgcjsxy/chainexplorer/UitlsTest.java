4da82bb2c330939274e691458552b25dc25e70ad
Rusrty <3130585774@qq.com>
2023-04-03 23:18:36 +0800

    demo

---
+++ app/src/test/java/com/hbgcjsxy/chainexplorer/UitlsTest.java
@@ -0,0 +1,27 @@
+package com.hbgcjsxy.chainexplorer;
+
+import junit.framework.TestCase;
+
+public class UitlsTest extends TestCase {
+
+    public void testTimestampToTime() {
+    }
+
+    public void testTimestampToTimeShort() {
+    }
+
+    public void testCalculateTransaction() {
+    }
+
+    public void testTransactionFillsParseRespond() {
+    }
+
+    public void testInfo_parseRespond() {
+    }
+
+    public void testBlockListParseRespond() {
+    }
+
+    public void testHttpsGetX() {
+    }
+}
